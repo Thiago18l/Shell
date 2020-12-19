e14615b759dd52841da40676762819fcb23dd2d3
thiago <thiago.lopes.dev@gmail.com>
2020-12-19 14:03:23 -0300

    chore: add first scripts

---
+++ source/script/number.sh
@@ -0,0 +1,3 @@
+x=10
+number=1
+echo $x, $number
---
+++ source/script/semantic.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#: Title    : SHELL
+#: Date     : 2020-12-19
+#: Author   : "Thiago Lopes Moreira" <thiago.lopes@trizy.com.br>
+#: Version  : 1.0
+#: Description : print Hello, World!
+#: Options  : None
+
+printf "%s\n" "Hello, World" "!"
