928645fea7f3280cc5452553b0f23f5bd379b7d5
thiago <thiago.lopes.dev@gmail.com>
2020-12-19 15:33:58 -0300

    chore: add positional and variables

---
+++ source/script/positional-parameters.sh
@@ -0,0 +1,15 @@
+
+printf "hello, %s!\n" "$1 thiago"
+echo "$*"
+
+# Variables
+
+name=VALUE
+echo "$name"
+
+
+echo 1, '2 3' 4 5
+echo -n Now\ is the time
+printf "%s %s\n" one two three
+
+bash --version
