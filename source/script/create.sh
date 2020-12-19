234608daaaa039aed28379caf405b7d1dbfb5868
thiago <thiago.lopes.dev@gmail.com>
2020-12-19 14:34:18 -0300

    new: a file that create a new directory and run a script

---
+++ source/script/create.sh
@@ -0,0 +1,11 @@
+pwd
+
+
+#! /bin/bash
+# Author    : Thiago Lopes
+
+mkdir /$HOME/bpl
+mkdir /$HOME/bpl/script
+mkdir /$HOME/bpl/bin
+
+# END
---
+++ source/script/executable.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# Author    : Thiago Lopes
+
+echo "echo Hello, World" > /$HOME/bpl/bin/hw.sh
+chmod +x /$HOME/bpl/bin/hw.sh
+/$HOME/bpl/bin/hw.sh
+
+# END
