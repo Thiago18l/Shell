e05c193ea2755d22dfb9049515a1c34700d1b1ad
thiago <thiago.lopes.dev@gmail.com>
2021-01-18 20:49:13 -0300

    chore: add format.sh

---
+++ source/script/format.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+# Author    : Thiago Lopes
+
+
+
+printf "color: #%02x%02x%02x; \n" 65 105 225
+
+# Width Specification
+
+printf "%8s %-15s:\n" First second third fourth fifth sixth
+
+printf "%04d\n" 12 23 56 123 255
+
+printf "%12.4s %9.2f\n" John 2 Jackson 4.578 Walter 2.9
