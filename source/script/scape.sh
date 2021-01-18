02be9067316d08028b388f15255aea8e0f0a8689
thiago <thiago.lopes.dev@gmail.com>
2021-01-18 20:42:42 -0300

    chore: add scape formatters

---
+++ source/script/scape.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# Author    : Thiago Lopes
+
+
+# Escape sequences are single letters preceded by a backslash
+
+printf "\a:: Alert\n"
+printf "\b: Backspace\n"
+printf "\e: escape character\n"
+printf "\f: Form feed\n"
+printf "\n: New line\n"
+printf "\r: Carriage return\n"
+printf "\t: Horizontal tab\n"
+printf "\v: Vertical tab\n"
+printf "\\ Backslash"
+printf "\nnn: A character specified by one to three octal digits\n"
+printf "\xHH: A character specified by one or two hexadecimal digits\n"
